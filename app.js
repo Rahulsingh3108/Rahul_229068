a8c88f62f6ede35f382214574279425c2805f74a
SurajK2025 <suraj.khokrale@rediffmail.com>
2023-01-20 16:48:54 +0530

    myexam

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const {join}=require('path');
+const path=require('path');
+
+const app=express();
+app.get("/",function(req,resp){
+resp.sendFile(path.join(__dirname,"/rahul.html"));
+})
+
+app.listen(3000);
+console.log("server listening on 3000");
